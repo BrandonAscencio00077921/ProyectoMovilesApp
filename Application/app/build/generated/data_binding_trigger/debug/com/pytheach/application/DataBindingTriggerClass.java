34c20ff01bef18e3538d4abf8c48dc7b7c1e3f89
BrandonAscencio00077921 <00077921@uca.edu.sv>
2023-07-01 11:40:18 -0600

    ui

---
+++ Application/app/build/generated/data_binding_trigger/debug/com/pytheach/application/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.pytheach.application;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ Application/app/build/generated/source/kapt/debug/com/pytheach/application/BR.java
@@ -0,0 +1,5 @@
+package com.pytheach.application;
+
+public class BR {
+  public static final int _all = 0;
+}
